a57657ffa5ff4d3aa90042e830c222e9269bc961
starTwingo <ibnat.072@gmail.com>
2025-09-22 23:48:00 +0600

    Solved 1004

---
+++ BEE1004 (Simple Product).js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('input.txt', 'utf8');
+var lines = input.split('\n');
+
+const A = parseInt(lines[0])
+const B = parseInt(lines[1])
+
+const PROD = A * B;
+console.log(`PROD = ${PROD}`);
